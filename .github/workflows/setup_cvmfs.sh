4fb98070ef0a81d749c1a549e0ddbb311a6bdaf3
iishiishii <love_sweet_98@yahoo.com.vn>
2023-07-14 09:15:59 +1000

    Stop and disable apt-daily upgrade services

--- .github/workflows/setup_cvmfs.sh
+++ .github/workflows/setup_cvmfs.sh
@@ -3,6 +3,22 @@ set -e
 
 export DEBIAN_FRONTEND=noninteractive
 
+# Stop and disable apt-daily upgrade services;
+systemctl stop apt-daily.timer
+systemctl disable apt-daily.timer
+systemctl disable apt-daily.service
+systemctl stop apt-daily-upgrade.timer
+systemctl disable apt-daily-upgrade.timer
+systemctl disable apt-daily-upgrade.service
+
+# This step should completely disable any automatic updates except manual
+sudo sed -i 's/APT::Periodic::Update-Package-Lists "1"/APT::Periodic::Update-Package-Lists "0"/' /etc/apt/apt.conf.d/20auto-upgrades
+
+# Enable retry logic for apt up to 10 times
+echo "APT::Acquire::Retries \"10\";" > /etc/apt/apt.conf.d/80-retries
+
+# Configure apt to always assume Y
+echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
 # uninstall unattended-upgrades
 sudo apt remove -y unattended-upgrades
 
